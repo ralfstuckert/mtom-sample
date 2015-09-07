a778073c777aae0f8424afe6260bdd2f0095276c
Ralf Stuckert <ralf.stuckert@googlemail.com>
2015-09-07 16:19:15 +0200

    use mimepull in order to allow streaming

--- mtom-server/src/main/java/rst/sample/mtom/server/Application.java
+++ mtom-server/src/main/java/rst/sample/mtom/server/Application.java
@@ -7,6 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
 	public static void main(String[] args) {
+		// needed for streaming, see https://java.net/jira/browse/SAAJ-31
+		System.setProperty("saaj.use.mimepull", "true");
+		
 		SpringApplication.run(Application.class, args);
 	}
 }
